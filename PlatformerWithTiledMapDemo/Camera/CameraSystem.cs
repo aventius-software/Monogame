71a283588f6e5660bd80fe4952cca374f5fcfb37
Ben Riley <47484806+aventius-software@users.noreply.github.com>
2025-10-24 13:54:53 +0100

    Added more comments to the camera system

--- PlatformerWithTiledMapDemo/Camera/CameraSystem.cs
+++ PlatformerWithTiledMapDemo/Camera/CameraSystem.cs
@@ -2,8 +2,9 @@
 using MonoGame.Extended;
 using MonoGame.Extended.ECS;
 using MonoGame.Extended.ECS.Systems;
+using MonoGame.Extended.Graphics;
 using PlatformerWithTiledMapDemo.Player;
-using System;
+using System.Linq;
 
 namespace PlatformerWithTiledMapDemo.Camera;
 
@@ -11,23 +12,41 @@ internal class CameraSystem : EntityProcessingSystem
 {
     private readonly OrthographicCamera _camera;
 
-    private ComponentMapper<PlayerComponent> _playerMapper;
+    private Vector2? _position = null;    
+    private Vector2 _playerSpriteSizeOffset;
     private ComponentMapper<Transform2> _transformMapper;
 
-    public CameraSystem(OrthographicCamera camera) : base(Aspect.All(typeof(PlayerComponent)))
+    public CameraSystem(OrthographicCamera camera) : base(Aspect.All(typeof(PlayerComponent), typeof(Transform2)))
     {
         _camera = camera;
     }
-    
+
     public override void Initialize(IComponentMapperService mapperService)
-    {
-        _playerMapper = mapperService.GetMapper<PlayerComponent>();
+    {        
         _transformMapper = mapperService.GetMapper<Transform2>();
+
+        // Get the player entity to determine the sprite size offset to center the camera. We
+        // divide by 2 as we want half the size to offset from the center. Then we can add this
+        // to the player's position to get the center point of the sprite. If we don't do this, the
+        // camera position will be offset by the entire sprite size, meaning the player will appear
+        // slightly off-center in the viewport.
+        var playerEntity = GetEntity(ActiveEntities.First());
+        _playerSpriteSizeOffset = playerEntity.Get<AnimatedSprite>().Size.ToVector2() / 2f;
     }
 
     public override void Process(GameTime gameTime, int entityId)
     {
+        // Get the player's transform, which contains the players current position
         var transform = _transformMapper.Get(entityId);
-        _camera.LookAt(transform.Position);
+        
+        // Interpolate the camera position for smooth movement between position
+        // changes, using a simple linear interpolation (lerp). This gives us a
+        // kind of an acceleration/deccelaration effect for the camera movement
+        // as it tracks towards the players position.
+        _position ??= _camera.Position;
+        _position = Vector2.Lerp((Vector2)_position, transform.Position + _playerSpriteSizeOffset, 0.25f);
+
+        // Update the camera to look at the new position
+        _camera.LookAt((Vector2)_position);
     }
 }
